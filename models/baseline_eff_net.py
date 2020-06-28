e4d36586673f20c5f8a173905a0c050886034afa
kento <18120134@student.hcmus.edu.vn>
2020-06-28 07:08:10 +0700

    change classifier

--- models/baseline_eff_net.py
+++ models/baseline_eff_net.py
@@ -43,17 +43,17 @@ class baseline_eff_net(nn.Module):
 
         self.feature_dim = self.extractor.feature_dim
 
-        self.cls_1 = nn.Linear(self.feature_dim, 640)
-        self.cls_2 = nn.Linear(640, 320)
-        self.cls_3 = nn.Linear(320, 160)
-        self.cls_4 = nn.Linear(160, 80)
-        self.cls_5 = nn.Linear(80, nclasses)
+        self.classifier = nn.Sequential(
+            nn.Linear(self.feature_dim, 1024),
+            nn.Dropout(0.2),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(1024, 512),
+            nn.Linear(512, nclasses)
+        )
 
     def forward(self, x):
         x = self.extractor.get_embedding(x)
-        x = F.relu(self.cls_1(x))
-        x = F.relu(self.cls_2(x))
-        x = F.relu(self.cls_3(x))
-        x = F.relu(self.cls_4(x))
-        x = self.cls_5(x)
+        x = self.classifier(x)
         return x
